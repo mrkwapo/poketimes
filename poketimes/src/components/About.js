8570722aba7405f8e13ee65af099f5d9ffe263da
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-14 01:56:44 -0400

    added navigation bar and links

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='container'>
+      <h4 className='center'>About</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A expedita
+        asperiores culpa unde totam consequuntur in ut deserunt, amet quasi,
+        error, quaerat nihil nostrum quia voluptatem porro rem fugit atque.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className='container'>
+      <h4 className='center'>Contact</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A expedita
+        asperiores culpa unde totam consequuntur in ut deserunt, amet quasi,
+        error, quaerat nihil nostrum quia voluptatem porro rem fugit atque.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
