c0d0a6969556f91d5beacce460c336dc8ed333be
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-05-17 23:27:44 -0400

    action creator

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import { connect } from 'react-redux';
+import { deletePost } from '../actions/postActions';
 
 class Post extends Component {
   handleClick = () => {
@@ -35,7 +36,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = dispatch => {
   return {
     deletePost: id => {
-      dispatch({ type: 'DELETE_POST', id: id });
+      dispatch(deletePost(id));
     }
   };
 };
